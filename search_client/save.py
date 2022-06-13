b95158b3b07a8cc35275e02f7572204a93998350
lestherll <ljllacuna5@gmail.com>
2022-06-14 00:37:35 +0100

    Add save to json

--- search_client/save.py
+++ search_client/save.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import csv
+import json
 
 
 def write_to_csv(tweets: list[dict[str, str]], filename: str) -> None:
@@ -10,12 +11,18 @@ def write_to_csv(tweets: list[dict[str, str]], filename: str) -> None:
         tweets (list[dict[str, str]]): list of tweets
         filename (str): to save to
     """
-    assert len(tweets) != 0, "tweets must not be 0"
+    assert len(tweets) != 0, "tweets must not be empty"
 
     with open(filename, "w", newline="") as csvfile:
         fieldnames = tweets[0].keys()
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        
+
         writer.writeheader()
         for t in tweets:
             writer.writerow(t)
+
+
+def write_to_json(tweets: list[dict[str, str]], filename: str) -> None:
+
+    with open(filename, "w") as jsonfile:
+        jsonfile.write(json.dumps(tweets, indent=4))
